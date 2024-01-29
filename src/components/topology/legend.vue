b3fcc13c997880b623bd844f77753d307a9209b6
zhangxin <812603872@qq.com>
2024-01-29 20:26:04 +0800

    fix

--- src/components/topology/legend.vue
+++ src/components/topology/legend.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="border-solid border border-blue-900 p-4">
     <div class="flex">
-      <div class="bg-red w-12 h-12 bg-contain bg-no-repeat"></div>
+      <div   :style="{ backgroundImage: `url(${imgUrl.legendFaluty})` }" class="w-12 h-12 bg-contain bg-no-repeat"></div>
       <div class="ml-4 text-lg font-bold">故障</div>
     </div>
     <div class="flex">
-      <div class="bg-gray-500 w-12 h-12 bg-contain bg-no-repeat"></div>
+      <div :style="{ backgroundImage: `url(${imgUrl.legendOffline})` }" class="w-12 h-12 bg-contain bg-no-repeat"></div>
       <div class="ml-4 text-lg font-bold">离线</div>
     </div>
     <div class="flex">
-      <div class="bg-green-600 w-12 h-12 bg-contain bg-no-repeat"></div>
+      <div :style="{ backgroundImage: `url(${imgUrl.legendMasterNormal})` }" class="w-12 h-12 bg-contain bg-no-repeat"></div>
       <div class="ml-4 text-lg font-bold">master在线</div>
     </div>
     <div class="flex">
-      <div class="bg-blue-600 w-12 h-12 bg-contain bg-no-repeat"></div>
+      <div :style="{ backgroundImage: `url(${imgUrl.legendOtherNormal})` }" class="w-12 h-12 bg-contain bg-no-repeat"></div>
       <div class="ml-4 text-lg font-bold">slave/relay在线</div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
+import imgUrl from './img'
 </script>
